73eae4f6d5dec186e2f15331b96b82f62000e57c
Yuta Ushijima <register@yuta-u.com>
2018-12-08 15:20:49 +0900

    CheckBoxの挙動

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-controls',
@@ -11,6 +12,7 @@ export class ControlsComponent implements OnInit {
   // テンプレートで呼び出すためのプロパティを定義
   coffeeForm: FormGroup;
   hotcoldsel = ['あったかーい', 'つめたーい', 'ぬるーい', 'ほっこーり'];
+  addsel = ['Milk', 'Sugar'];
   constructor(
     private fb: FormBuilder
   ) { }
@@ -19,8 +21,23 @@ export class ControlsComponent implements OnInit {
     this.coffeeForm = this.fb.group({
       name: 'ブレンド',
       taste: 'バランスのよい口当たり',
-      hotcold: this.hotcoldsel[0]
+      hotcold: this.hotcoldsel[0],
+      addsel: this.fb.array([])
     });
   }
 
+  // チェックボックスの処理
+  onCheckChanged(item: string, isChecked: boolean) {
+    // addsの状態を取得
+    const formArray = <FormArray>this.coffeeForm.controls.addsel;
+      if (isChecked) {
+        // チェックがついているときは、formArrayに要素を追加
+        formArray.push(new FormControl(item));
+      } else {
+          // チェックがついていないときは、formArrayから要素を削除
+          const index = formArray.controls.findIndex(
+            element => element.value === item);
+          formArray.removeAt(index);
+        }
+      }
 }
