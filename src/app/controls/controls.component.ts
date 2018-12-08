f57f093839fa7ce8d80a12397599eb9e48d43229
Yuta Ushijima <register@yuta-u.com>
2018-12-08 15:42:41 +0900

    チェックボックスの実装

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -11,25 +11,27 @@ export class ControlsComponent implements OnInit {
 
   // テンプレートで呼び出すためのプロパティを定義
   coffeeForm: FormGroup;
-  hotcoldsel = ['あったかーい', 'つめたーい', 'ぬるーい', 'ほっこーり'];
-  addsel = ['Milk', 'Sugar'];
+  hotColdSel = ['あったかーい', 'つめたーい', 'ぬるーい', 'ほっこーり'];
+  addSel = ['Milk', 'Sugar'];
+  nutSel = ['ピーナッツ', 'アーモンド', 'くるみ'];
   constructor(
     private fb: FormBuilder
   ) { }
 
   ngOnInit() {
     this.coffeeForm = this.fb.group({
-      name: 'ブレンド',
+      name: 'ブレンドC',
       taste: 'バランスのよい口当たり',
-      hotcold: this.hotcoldsel[0],
-      addsel: this.fb.array([])
+      hotCold: this.hotColdSel[0],
+      adds: this.fb.array([]),
+      nut: this.nutSel[0]
     });
   }
 
   // チェックボックスの処理
   onCheckChanged(item: string, isChecked: boolean) {
     // addsの状態を取得
-    const formArray = <FormArray>this.coffeeForm.controls.addsel;
+    const formArray = <FormArray>this.coffeeForm.controls.adds;
       if (isChecked) {
         // チェックがついているときは、formArrayに要素を追加
         formArray.push(new FormControl(item));
